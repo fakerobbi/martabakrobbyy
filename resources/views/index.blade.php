aee8ce655c7c722ee05f13b95170027e5af259ef
fakerobbi <robbybs003@gmail.com>
2020-07-10 00:37:20 +0700

    Ubah dikit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -109,7 +109,7 @@
         <p><strong> buat kamu yang lagi males keluar tapi pengen nyobain Martabak Robby, 
           tenang aja sekarang Martabak Robby sudah hadir diGofood jadi jangan sampai kehabisan ya!</strong></p>
           <a href="https://gofood.link/u/ejLO9"><button class="btn btn-custom btn-lg">Order Now</button></a><br>
-          <img src="{{asset('img/')}}/gofood.png" width="20%" alt="">   
+          <img src="{{asset('img//gofood.png')}}" width="20%" alt="">   
       </div>
     </div>
   </div>
