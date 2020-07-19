1aaa6c21c3ae6d7b1cea2277c31f85cf6f82228f
fakerobbi <robbybs003@gmail.com>
2020-07-19 16:31:28 +0700

    ubah lagi

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -5,7 +5,7 @@
           <div class="row">
             <div class="intro-text">
               <h1>Martabak Robby</h1>
-              <p>Eat martabak everyday can make ur happy.</p>
+              <p>Eat martabak everyday can make u happy.</p>
               <a href="#about" class="btn btn-custom btn-lg page-scroll">Get Started</a> </div>
           </div>
         </div>
